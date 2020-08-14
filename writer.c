9b8e9971073e2cb4e6d0a3201cb77b3f623c899a
Kevin Moncayo <nano2916@gmail.com>
2020-08-13 23:15:07 -0300

    Reconocimiento señales SIGUSR1 & SIGUSR2

--- writer.c
+++ writer.c
@@ -7,9 +7,26 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <stdint.h>
+#include <signal.h> // Include this library to make management of Signals
 
 #define FIFO_NAME "myfifo"
 #define BUFFER_SIZE 300
+
+void my_handler (int signum)
+{
+    if (signum == SIGUSR1)
+    {
+        printf("Recibido SIGUSR1!\n");
+    }
+}
+
+void my_handler2 (int signum2)
+{
+    if (signum2 == SIGUSR2)
+    {
+        printf("Recibido SIGUSR2!\n");
+    }
+}
 
 
 int main(void)
@@ -17,9 +34,12 @@ int main(void)
    
     char outputBuffer[BUFFER_SIZE];
     char outputBufferPrefix[BUFFER_SIZE]={"DATA: "};
+    char outputBufferPrefix2[BUFFER_SIZE]={"SIGN:1"};
+    char outputBufferPrefix3[BUFFER_SIZE]={"SIGN:2"};
 
 	uint32_t bytesWrote;
 	int32_t returnCode, fd;
+    int32_t returnCode2, returnCode3; // Variable that storage management error from SIGUSR1 and SIGUSR2
 
     /* Create named fifo. -1 means already exists so no action if already exists */
     if ( (returnCode = mknod(FIFO_NAME, S_IFIFO | 0666, 0) ) < -1 )
@@ -44,6 +64,24 @@ int main(void)
 	{
         /* Get some text from console */
 		fgets(outputBuffer, BUFFER_SIZE, stdin);
+
+        //This detects me when someone sends SIGUSR1 signal
+        if (returnCode2 = signal(SIGUSR1, my_handler) < -1)
+        {
+            perror("Error");
+        }
+        else
+        {
+            printf("Valor returnCode2 = %d\n",returnCode2);
+        }
+        
+        
+        //This detects me when someone sends SIGUSR2 signal
+                 
+        if (returnCode3 = signal(SIGUSR2, my_handler2) < -1)
+        {
+            perror("Error");
+        }
 
         //Put prefix "DATA:" in BUFFER_SIZE
         strcat(outputBufferPrefix,outputBuffer); 
